406372e662b2d538893ba9a06318b1a6f2a5d01d
MohammadMehdiMojrian <mohammadmahdimojrian@gmail.com>
2024-03-05 14:24:30 +0330

    Update q1.py

--- examples/Q1/q1.py
+++ examples/Q1/q1.py
@@ -1,14 +1,14 @@
 a=input()
 n=len(a)-1
 if a[0]=='[' and a[n]==']':
-    print(type(eval(a)))
+    print(type(eval(a)),eval(a))
     exit()
 if ord(a[0])==34 or ord(a[0])==39:
-    print(type(a))
+    print(type(a),a)
     exit()
 for i in a:
     if ord(i)>=65 and ord(i)<=122:
-        print(type(a))
+        print(type(a),a)
         exit()
 if type(eval(a)=='int' or eval(a)=='float'):
-    print(type(eval(a)),'number')
+    print(type(eval(a)),eval(a),'number')
